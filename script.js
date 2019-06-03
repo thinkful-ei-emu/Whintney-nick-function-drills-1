fb8daf591a44bf6604e65c56ecedbef8e20aafb6
Nicholas Gunter <ygnick@live.com>
2019-06-03 15:37:49 -0400

    checked for params in function

--- script.js
+++ script.js
@@ -7,16 +7,20 @@ function getYearOfBirth(age) {
 }
 
 function createGreeting(name, age) {
+  if (typeof name === 'string' && typeof age === 'number'){
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I am ${age}.  I was born in ${yearOfBirth}.`;
+  } else {
+    throw new Error('Arguments not valid')
+  }
 }
 
 try {
   const greeting = createGreeting('Nick', -1);
 } catch(e){
-  console.error(error);
+  console.error('error');
 }
 
-const greeting = createGreeting('Nick', 24);
+const greeting = createGreeting('Nick');
 
 console.log(greeting);
