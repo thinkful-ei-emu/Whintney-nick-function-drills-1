13c6c7081ac127f50db31b88996b3df45062f52d
Whitney Wallace <whitneysamwallace@gmail.com>
2019-06-03 12:07:26 -0700

    created new function

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 'use strict';
+function getYearOfBirth(age) {
+  return (2019 - age);
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = (2019 - age);
+  const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I am ${age}.  I was born in ${yearOfBirth}.`;
 }
 
-const greeting = createGreeting();
+
+
+const greeting = createGreeting('Nick', 24);
 
 console.log(greeting);
